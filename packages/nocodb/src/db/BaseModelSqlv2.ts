3a55aadb7f4d9c922fd0f64e2ba65b39e823c3c2
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2026-03-02 09:37:24 +0000

    fix: duplicate ai field issue

--- packages/nocodb/src/db/BaseModelSqlv2.ts
+++ packages/nocodb/src/db/BaseModelSqlv2.ts
@@ -37,6 +37,7 @@ import {
   PermissionKey,
   RelationTypes,
   UITypes,
+  parseHelper,
 } from 'nocodb-sdk';
 import { v4 as uuidv4 } from 'uuid';
 import debug from 'debug';
@@ -7162,7 +7163,9 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
         }
       } else if (isAIPromptCol(column) && !extra?.raw) {
         if (data[column.column_name]) {
-          let value = data[column.column_name];
+          // value can be stringified object or string
+          let value = parseHelper(data[column.column_name]);
+
           /**
            * IsAiEdited is used to fix edited by ai issue in expanded form as cookie?.system will be undefined in that case
            */
@@ -7172,7 +7175,7 @@ class BaseModelSqlv2 implements IBaseModelSqlV2 {
             isAiEdited = value.isAiEdited;
             delete value.isAiEdited;
 
-            value = value.value;
+            value = value.value ?? '';
           }
 
           const obj: {
